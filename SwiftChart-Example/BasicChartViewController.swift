8c4984a6a184ddeca4770605a34021058369c64f
Peter Salz <psalz.app.dev@web.de>
2019-08-18 21:50:08 +0200

    Added empty source files.

---
+++ SwiftChart-Example/BasicChartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BasicChartViewController.swift
+//  SwiftChart-Example
+//
+//  Created by Peter Salz on 18.08.19.
+//  Copyright © 2019 Peter Salz App Development. All rights reserved.
+//
+
+import UIKit
+
+class BasicChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftChart-Example/StockChartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StockChartViewController.swift
+//  SwiftChart-Example
+//
+//  Created by Peter Salz on 18.08.19.
+//  Copyright © 2019 Peter Salz App Development. All rights reserved.
+//
+
+import UIKit
+
+class StockChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
